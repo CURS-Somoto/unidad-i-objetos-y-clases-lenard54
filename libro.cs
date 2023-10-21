df637ee91b4040de9cf70e1f33a7d404e2aa6f49
Binary_Life <147262828+lenard54@users.noreply.github.com>
2023-10-20 19:52:43 -0600

    Update libro.cs
    Entrega de trabajo

--- libro.cs
+++ libro.cs
@@ -6,7 +6,12 @@ public class Libro
     public string Título { get; set; }
     public string Autor { get; set; }
 
-  
+    public void Mostrarinformación() 
+    {
+     System.Console.WriteLine("Titulo: " + titulo);
+     System.Console.WriteLine("Autor: " + autor);
+     System.Console.WriteLine(" ");
+    }
    
 }
 
@@ -14,6 +19,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("¡Hola, mundo!");
+        Libro libro1 = new Libro();
+        libro1.titulo = "EL QUIJOTE";
+        libro1.autor = "Miguel de Cervantes";
+
+        Libro libro2 = new Libro();
+        libro2.titulo = "LA ODISEA";
+        libro2.autor = "Homero";
+
+        libro1.Mostrarinformación();
+        libro2.Mostrarinformación();
+
     }
 }
