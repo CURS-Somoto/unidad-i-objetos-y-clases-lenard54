416788edb2e1822e6f98aa2d201b89ad51a6c0f3
Binary_Life <147262828+lenard54@users.noreply.github.com>
2023-10-20 19:28:58 -0600

    Update estudiante.cs
    Entrega de trabajo

--- estudiante.cs
+++ estudiante.cs
@@ -7,7 +7,12 @@ public class Estudiante
     public int Edad { get; set; }
     public string Curso { get; set; }
 
-    
+     public Estudiante(string nombre, int edad, string curso)
+    {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.curso = curso;
+    }
    
 }
 
@@ -15,6 +20,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("¡Hola, mundo!");
+       Estudiante estudiante1 = new Estudiante("Jorge", 18,
+    "ingenieria en sistemas.");
+  
+      Estudiante estudiante2 = new Estudiante("Carlos", 17,
+    "derecho.");
+
+      Estudiante estudiante3 = new Estudiante("Cristal", 20,
+    "enfermeria.");
+
+      System.Console.WriteLine($"Mi nombre es {estudiante1.nombre} tengo {estudiante1.edad} años y curso la carrera de {estudiante1.curso}");
+      System.Console.WriteLine($"Mi nombre es {estudiante2.nombre} tengo {estudiante2.edad} años y curso la carrera de {estudiante2.curso}");
+      System.Console.WriteLine($"Mi nombre es {estudiante3.nombre} tengo {estudiante3.edad} años y curso la carrera de {estudiante3.curso}");
+
     }
 }
